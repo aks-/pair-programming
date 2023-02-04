495c782b3d3fd35bcc0f4089e7e24793ce105424
Ashok Suthar <coderatlabs@gmail.com>
2023-02-04 20:19:46 +0100

    Add movies endpoints to server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const app = express();
+const movies = require("./movies.json");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => res.status(200).send("OK"));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  return res.json(movies);
+});
+
+app.get("/:movieName", function (req, res) {
+  const { movieName: name } = req.params;
+
+  const movie = movies.filter(function (movie) {
+    return String(movie.name).toLowerCase() === String(name).toLowerCase();
+  });
+
+  return res.json(movie);
+});
+
+// TODO handle errors
+app.post("/:movieName", function (req, res) {
+  const { movieName: name } = req.params;
+  const { imdb, cast } = req.body;
+
+  const newMovies = [
+    ...movies,
+    {
+      name,
+      imdb,
+      cast,
+    },
+  ];
+
+  return fs.writeFile(
+    "./movies.js",
+    JSON.stringify(newMovies),
+    function (err, _) {
+      return res.json(newMovies);
+    }
+  );
+});
 
 app.listen(3000);
